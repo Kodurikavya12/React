da3e50c42db15fd0cb654c28b99ddfcd2cc8672d
Kodurikavya12 <kodurikavya30@gmail.com>
2026-04-29 09:51:52 +0530

    feat : added About, projects,Skills,Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 
-import Login from './Components/Login'
+import Projects from './Components/Projects'
 import Home from './Components/Home'
 import About from './Components/About'
 import Contact from './Components/Contact'
+import Skills from './Components/Skills'
 function App() {
   
 
   return (
-    <div>
-      
+    <> 
       <Home/>
       <About/>
       <Contact/>
-      <Login/>
+      <Projects/>
+      <Skills/>
       
-    </div>
+      
+    </>
   )
 }
 
--- src/Components/Login.jsx
+++
@@ -1,6 +0,0 @@
-function Login() {
-    return (
-        <h3>Login</h3>
-    )
-}
-export default Login
